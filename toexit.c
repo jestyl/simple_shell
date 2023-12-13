c8e849f2117b79fe274f1ff4daab9844ea65944d
di-creator <131366061+di-creator@users.noreply.github.com>
2023-12-13 22:59:00 +0100

    Create toexit.c

---
+++ toexit.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+ **_strncpy - Copies a specified number of characters from the source string to the destination string.
+ *@dest: the destination string will be copied
+ *@src: this is the source string
+ *@n: this is the number of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
