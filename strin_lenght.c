18b2453bfc83184857ffb70abf51eec0fe412d2e
di-creator <131366061+di-creator@users.noreply.github.com>
2023-12-14 01:36:33 +0100

    Update strin_lenght.c

--- strin_lenght.c
+++ strin_lenght.c
@@ -18,7 +18,14 @@ int _strlen(char *s)
 	return (i);
 }
 
-
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string for comparison.
+ * @s2: The second string for comparison.
+ *
+ * Return: Returns an integer less than, equal to, or greater than zero
+ *         if s1 is found to be less than, equal to, or greater than s2, respectively.
+ */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
@@ -33,8 +40,12 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (*s1 < *s2 ? -1 : 1);
 }
-
-
+/**
+ * starts_with - Checks if a string starts with a specified prefix.
+ * @haystack: The string to check.
+ * @needle: The prefix to look for.
+ * Return: Returns a pointer 
+ */
 char *starts_with(const char *haystack, const char *needle)
 {
 	while (*needle)
@@ -43,7 +54,12 @@ char *starts_with(const char *haystack, const char *needle)
 	return ((char *)haystack);
 }
 
-
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string to be appended.
+ * Return: Returns a pointer to the concatenated string (dest).
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
